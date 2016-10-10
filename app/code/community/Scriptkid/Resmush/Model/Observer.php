dadf5d9f3a9c8635e146129e8334fbbe807a4c6a
graphodata AG <team@graphodata.de>
2016-10-10 11:12:06 +0200

    forgot to add in the observer.php...

---
+++ app/code/community/Scriptkid/Resmush/Model/Observer.php
@@ -0,0 +1,11 @@
+<?php
+
+class Scriptkid_Resmush_Model_Observer
+{
+
+  public function catalogProductGalleryUploadImageAfter($observer)
+  {
+      //do cool stuff with the image uploaded
+  }
+
+}
