b9efee165e0e23c8212730cdb5ae04e325c44580
graphodata AG <team@graphodata.de>
2016-10-10 11:55:05 +0200

    Found some info to work with in the event result

--- app/code/community/Scriptkid/Resmush/Model/Observer.php
+++ app/code/community/Scriptkid/Resmush/Model/Observer.php
@@ -3,9 +3,17 @@
 class Scriptkid_Resmush_Model_Observer
 {
 
+  /*
+  *   Watches catalog_product_gallery_upload_image_after and has $this and $result in the event.
+  *
+  */
   public function catalogProductGalleryUploadImageAfter($observer)
   {
-      //do cool stuff with the image uploaded
+      $result = $observer->getEvent()->getResult();
+      $_imageName = $_result['name'];
+      $_imageFilepath = $_result['path'].$_result['file'];
+      var_dump($result);
+      die();
   }
 
 }
